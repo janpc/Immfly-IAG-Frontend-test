869edd30102607a286f98e3f5775bf942ec61d5b
Jan Puig <janpuigcapella@gmail.com>
2021-07-20 20:29:42 +0200

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
 
-function App() {
-  return <p>Hello world!</p>;
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/pokemon/:name">
+          <p>pokemon</p>
+        </Route>
+        <Route path="/pokemon">
+          <p>list</p>
+        </Route>
+        <Route path="/">
+          <Redirect to="/pokemon" />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
-
-export default App;
